a384b806786c154efc2e7b57e0de88bd75d38636
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-08-26 10:53:55 +0530

    hotfix

--- src/constants/avatar.js
+++ src/constants/avatar.js
@@ -1,1 +1,1 @@
-export const PROFILE_IMAGE = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+export const PROFILE_IMAGE = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
